cc931165613f5f654bf8a78ebf63166a40a78420
Kanan Rahimov <mail@kenanbek.me>
2019-08-01 18:36:24 +0200

    Survey component (not finished)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navigation from '../navigation/Navigation';
 import SurveyList from '../surveylist/SurveyList';
+import Survey from '../survey/Survey';
 
 function App() {
   return (
     <div>
       <Navigation />
       <SurveyList />
+      <Survey />
     </div>
   );
 }
---
+++ src/components/survey/Survey.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Form from 'react-jsonschema-form';
+
+class Survey extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      schema: null,
+    };
+  }
+
+  async componentDidMount() {
+    const schema = (await axios.get('http://localhost:8000/api/v1/surveys/10/')).data.questions;
+    this.setState({
+      schema,
+    });
+  }
+
+  render() {
+    const { schema } = this.state;
+    const log = type => console.log.bind(console, type);
+
+    if (schema != null) {
+      return (
+        <Form
+          schema={schema}
+          onChange={log('changed')}
+          onSubmit={log('submitted')}
+          onError={log('errors')}
+        />
+      );
+    }
+    return (
+      <p>Loading..,</p>
+    );
+  }
+}
+
+export default Survey;
