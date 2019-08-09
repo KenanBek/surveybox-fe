6a346305c8e6aa8b8dabb8b6dea3e2d85f26ec49
Kanan Rahimov <mail@kenanbek.me>
2019-08-09 15:34:12 +0200

    Initial tests for async and normal actions

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,13 +1,45 @@
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import MockAdapter from 'axios-mock-adapter';
+
 import * as actions from './actions';
 import C from './constants';
 
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const axiosMock = new MockAdapter(axios);
+
 describe('actions', () => {
-  it('should create an action to add a todo', () => {
+  it('should create an action to add an error', () => {
     const text = 'error1';
     const expectedAction = {
       type: C.ADD_ERROR,
       payload: text,
     };
     expect(actions.addError(text)).toEqual(expectedAction);
+  });
+});
+
+describe('async actions', () => {
+  afterEach(() => {
+    axiosMock.restore();
+  });
+
+  it('creates FETCH_SURVEY_LIST when fetching surveys has been done', () => {
+    axiosMock.onGet('api/v1/surveys/').reply(200, {
+      results: [],
+    });
+
+    const store = mockStore({});
+    const expectedActions = [
+      C.FETCH_SURVEY_LIST,
+    ];
+
+    return store.dispatch(actions.fetchSurveyList())
+      .then(() => {
+        const actualActions = store.getActions().map(action => action.type);
+        expect(actualActions).toEqual(expectedActions);
+      });
   });
 });
